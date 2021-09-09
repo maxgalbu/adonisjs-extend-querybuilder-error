a5bcc720964023caa076b3c5f94641057f0e421f
Massimo Galbusera <massimo.galbusera@hotelscan.com>
2021-09-09 09:47:47 +0200

    fix slugify

--- start/functions.ts
+++ start/functions.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function slugify(text:string) {
-  const {data: {slug}} = await axios.get("http://slugify.adresa.info/json?string=ciao%20bello");
+  const {data: {slug}} = await axios.get("http://slugify.adresa.info/json?string="+encodeURIComponent(text));
   return slug;
 }
