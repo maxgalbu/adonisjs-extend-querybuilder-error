e1de9a2279fb332ab7477c01eaf5cc19881ab98e
Massimo Galbusera <massimo.galbusera@hotelscan.com>
2021-09-09 11:05:06 +0200

    I promise this:

--- contracts/database.ts
+++ contracts/database.ts
@@ -1,5 +1,5 @@
 declare module '@ioc:Adonis/Lucid/Orm' {
   interface ModelQueryBuilderContract<Model extends LucidModel, Result = InstanceType<Model>> {
-    whereSlug(column:string, value:string): this; //see error in declaration in providers/AppProvider.ts
+    whereSlug(column:string, value:string): Promise<this>; //see error in declaration in providers/AppProvider.ts
   }
 }
